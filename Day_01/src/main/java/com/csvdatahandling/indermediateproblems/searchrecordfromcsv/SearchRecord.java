523c50c77f2adf1dd00f20eb266fe2605e8bebb0
viveksahu06 <viveksahu1006@gmail.com>
2025-02-18 22:42:43 +0530

    [Vivek] Added Search for a Record in CSV

---
+++ Day_01/src/main/java/com/csvdatahandling/indermediateproblems/searchrecordfromcsv/SearchRecord.java
@@ -0,0 +1,10 @@
+package com.csvdatahandling.indermediateproblems.searchrecordfromcsv;
+
+public class SearchRecord {
+    public static void main(String[] args) {
+        // file path
+        String filePath ="src/main/java/com/csvdatahandling/indermediateproblems/searchrecordfromcsv/Employee.csv";
+        // calling method to search the record based on the employee name
+        SearchRecordAndPrint.searchAndPrint(filePath);
+    }
+}
---
+++ Day_01/src/main/java/com/csvdatahandling/indermediateproblems/searchrecordfromcsv/SearchRecordAndPrint.java
@@ -0,0 +1,37 @@
+package com.csvdatahandling.indermediateproblems.searchrecordfromcsv;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SearchRecordAndPrint {
+    public static void searchAndPrint(String filepath){
+        // try with resource
+        //reading the file and searching the employee name data
+           try(CSVReader reader = new CSVReader(new FileReader(filepath))){
+               String []row ;
+               Scanner sc = new Scanner(System.in);
+               System.out.print("Enter the employee name to get their data : ");
+               String emplyoeeName =sc.nextLine();
+               boolean flag =true;
+               while((row=reader.readNext())!=null){
+                  if(emplyoeeName.equals(row[1])){
+                      System.out.println("ID : "+row[0]+","+" NAME : "+row[1]+","+"DEPARTMENT : "+row[2]+","+"SALARY : "+row[3]);
+                  flag =false;
+                  }
+               }
+               if(flag){
+                   System.out.println("Record not found");
+               }
+           }catch(FileNotFoundException f){
+               f.getMessage();
+           }catch(IOException e){
+               e.printStackTrace();
+           }catch(Exception e){
+               e.printStackTrace();
+           }
+    }
+}
