2a15400e13bc100869a952bae0cdcc04d4532748
viveksahu06 <viveksahu1006@gmail.com>
2025-02-18 22:43:53 +0530

    [Vivek] Added Sort CSV Records by a Column

---
+++ Day_01/src/main/java/com/csvdatahandling/indermediateproblems/sortcsvrecord/MyComparator.java
@@ -0,0 +1,14 @@
+package com.csvdatahandling.indermediateproblems.sortcsvrecord;
+
+import java.util.Comparator;
+
+// customize sorting is done here
+class MyComparator implements Comparator<String[]> {
+    @Override
+    public int compare(String[] s1, String[] s2) {
+        double salary1 = Double.parseDouble(s1[3]);
+        double salary2 = Double.parseDouble(s2[3]);
+        return Double.compare(salary1, salary2);
+    }
+}
+
---
+++ Day_01/src/main/java/com/csvdatahandling/indermediateproblems/sortcsvrecord/SortRecordBasedOnSalary.java
@@ -0,0 +1,46 @@
+package com.csvdatahandling.indermediateproblems.sortcsvrecord;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+public class SortRecordBasedOnSalary {
+    public static void main(String[] args) {
+
+        String filepath ="src/main/java/com/csvdatahandling/indermediateproblems/sortcsvrecord/Employee.csv";
+        MyComparator c = new MyComparator();
+        Set<String[]> set=new TreeSet<>(c);
+        // try block with resource
+        try(CSVReader reader =new CSVReader(new FileReader(filepath)))
+        {
+
+            String []row ;
+
+
+            while((row=reader.readNext())!=null){
+
+                // adding each row to set
+                set.add(row);
+            }
+            System.out.println("Employee data sorted based on salary");
+        }
+        catch(FileNotFoundException f){
+            f.getMessage();
+        }catch(IOException e){
+            e.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        // try block to add to the list
+        try(CSVWriter write = new CSVWriter(new FileWriter(filepath))){
+            write.writeAll(set);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
